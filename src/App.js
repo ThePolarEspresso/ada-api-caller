e55a1cd1b2194fc3513b82b341dafc2334b9f604
Anna <ajcarvalho1201@gmail.com>
2022-06-06 12:02:22 -0400

    cleaned up some code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const adaUrl = 'https://api.dev.alldataapp.com';
-const bearerToken = 'Bearer eyJraWQiOiJsdzFKcGdLNThEcThZNXJTSk9vQ1wvZnk0V1V0TVVcL1BQcTBMQ1d0cVlkWjA9IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiIwYjM4YjE2Yy0yYzI1LTRjNTMtOTFlMi1hNmYwOWIyZTUzMGUiLCJjb2duaXRvOmdyb3VwcyI6WyJ2aW4tbWFuYWdlci1uYSIsImNyZWF0b3ItZXUiLCJ2aW4tbWFuYWdlci1ldSIsImNyZWF0b3ItbmEiXSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMi5hbWF6b25hd3MuY29tXC91cy1lYXN0LTJfZU5MSmhMeWtwIiwidmVyc2lvbiI6MiwiY2xpZW50X2lkIjoiM3Budm5ldnBia2RxMWgyNHBtM201YjMzZzciLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6ImF3cy5jb2duaXRvLnNpZ25pbi51c2VyLmFkbWluIG9wZW5pZCBwcm9maWxlIGVtYWlsIiwiYXV0aF90aW1lIjoxNjU0NTIzNTAxLCJleHAiOjE2NTQ2MDk5MDEsImlhdCI6MTY1NDUyMzUwMSwianRpIjoiYTg1MzFlMjktZWRjNi00OWVjLTg5NDYtYTAxNmM4ZDRjZDE3IiwidXNlcm5hbWUiOiJHb29nbGVfMTA1NjcwOTE1NjM4NzYwMzQzMDQ3In0.iCKqyYyNO39pUVMcBA5KMlGOV4GJXlpjFS_CPCfLg6koo7lDq_uhLEn1x0Ddfhi284aeiyWu8Y1C81Bw2wOCbl7rEm6maBKWEOG90q_lXPCXh9Er9CX9jCZKb3na3xU-L5Kv4jHc-n82Brt4gL8HI0dr4M1GTClnxvT9UHiFhBNQt3kY0D1spxr3VZvC8NCHoS_cpBmo5lgDsWtB584wZia2y9HvcI6kFZZkgZLAWrgX1cMTWY4pyiXMIokdY-8oj0ab4HCur4dt54i4JtVnylv0sjMQrVWsWSg7HNUuOuTKKt6caQlu1xpw5HxPTaztcsZArwLdOftuAL6QiVOW8w';
+const bearerToken = 'Bearer eyJraWQiOiJaYmJJZXZIQ3F4RmdhV0Rva2RVQ1ZUTUhcL1N3U1dMaHlpR0hwdnJzVGdiMD0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJlMzIwYzZjNy01YjhiLTQxZmQtYmIxOC0yODFlMTJhNmVlZTUiLCJjb2duaXRvOmdyb3VwcyI6WyJhZG1pbi1ldSIsImFkbWluLW5hIiwiY3JlYXRvci1uYSJdLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0yLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMl95ZzdxenpLelQiLCJjbGllbnRfaWQiOiIzZjZwbDhvdXQ3a2ZqZGdxMWhuYjEzMmtsNiIsImV2ZW50X2lkIjoiNTNjZDYwNjMtMGI2My00YTY5LWIzMmQtMGZkZGU1YWM4NWQ3IiwidG9rZW5fdXNlIjoiYWNjZXNzIiwic2NvcGUiOiJhd3MuY29nbml0by5zaWduaW4udXNlci5hZG1pbiIsImF1dGhfdGltZSI6MTY1NDUyNzcxNywiZXhwIjoxNjU0NjE0MTE3LCJpYXQiOjE2NTQ1Mjc3MTcsImp0aSI6ImVkZmM0OTc0LTY3YmUtNDRmMS1iZjlmLWRiMzdlMzEwODg2ZCIsInVzZXJuYW1lIjoiYW5uYS5jYXJ2YWxob0BzdGVsbGFudGlzLmNvbSJ9.IlocvCYr4xMjhfwvHPwH9Z99nEzqHC3lUWLg2QMndY5zpFRI0HRkIhKPxoFMFapTLHeNVzg03x7Eof2bGUT0hucvqAHEmDmTkGnxvWP-SujopmUP57Z9UuPU-ngtqC4_fNqD5kiCW0WLRtdbfDN8FsHq6GHVLHO5vtxotkP1uB_cSHlMtilgPnA0McdS_BuKT0QxplhHQPYCd7ErZkSmFMmbo-eOIHHm8CBDK-AK_uhoi6fnWbl-hY76GZ3MBaznqKgqiVQov7U7DJwYojqjPi5FFKtYmClztnBy3tkUveLmEelQF3aLW0uPopM_XR60f52dF40o2gzTv_TjtqwAoQ';
 const vin = '1C4SDJET7KC500079';
 
 const vinDetails = `/v1/vin/${vin}`;
@@ -9,8 +9,7 @@ const lastKnown = `/v1/vin/${vin}/data/lastknown`;
 
 let urlToFetch = `${adaUrl}${lastKnown}`;
 
-function App() {
-
+export default function App() {
   let req = {
     method: 'get',
     url: urlToFetch,
@@ -19,13 +18,15 @@ function App() {
     }
   }
 
+  getData(req)
+}
+
+function getData(req) {
   axios(req)
-  .then((response) => {
-    console.log(response.data);
-    document.write(JSON.stringify(response.data))
+  .then((res) => {
+    console.log(res.data);
+    document.write(JSON.stringify(res.data))
   }, (error) => {
     console.log(error)
   });
 }
-
-export default App;
